98271396a1451c6c53e4004ef6f182a6d1e335cb
Unknown <andrew1gar@yandex.ru>
2017-11-09 01:00:39 +0300

    ver 0.1

---
+++ core/core.php
@@ -0,0 +1,34 @@
+<?php
+require_once 'functions.php';
+$homeWorkNum = '4.4';
+$homeWorkCaption = 'Управление таблицами и базами данных.';
+
+const HOST = 'localhost';
+const DB = 'global';
+const USER = 'andrew';
+const PASS = '2002';
+$tableName = 'table';
+
+/**
+ * Устанавливаем системные настройки
+ */
+ini_set("display_errors", "1"); // Показ ошибок
+ini_set("display_startup_errors", "1");
+ini_set('error_reporting', E_ALL);
+mb_internal_encoding('UTF-8'); // Кодировка по умолчанию
+
+/**
+ * Загружаем классы
+ */
+spl_autoload_register(function($name) {
+    $file = dirname(__DIR__) . '/core/' . $name . '.class.php';
+    if (!file_exists($file)) {
+        throw new Exception('Autoload class: File ' . $file . ' not found');
+    }
+    require $file;
+});
+
+/**
+ * Создаем объект
+ */
+$dataBase = new DataBase($tableName);
